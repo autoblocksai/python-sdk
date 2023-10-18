c1c3cd9f93e9a7ccd0014a2c1e7fa2d0f3428a35
Nicole White <nicole@autoblocks.ai>
2023-10-18 08:06:54 -0400

    Give access to underlying tracer (#29)

--- tests/vendor/langchain/test_callback.py
+++ tests/vendor/langchain/test_callback.py
@@ -1,7 +1,9 @@
 import json
 import os
+import uuid
 from unittest import mock
 
+import pytest
 from langchain.agents import AgentType
 from langchain.agents import initialize_agent
 from langchain.chains import LLMMathChain
@@ -20,12 +22,78 @@ def decode_requests(requests):
     return [json.loads(req.content.decode()) for req in requests]
 
 
-def test_callback_handler(httpx_mock):
-    httpx_mock.add_response(json=dict(traceId="mock-trace-id"))
-
+@pytest.fixture
+def handler():
     with mock.patch.dict(os.environ, {AUTOBLOCKS_INGESTION_KEY: "mock-ingestion-key"}):
-        handler = AutoblocksCallbackHandler()
+        return AutoblocksCallbackHandler()
 
+
+@pytest.fixture
+def httpx(httpx_mock):
+    httpx_mock.add_response(json=dict(traceId="mock-trace-id"))
+    return httpx_mock
+
+
+def test_callback_sends_langchain_version(httpx, handler):
+    llm = OpenAI()
+    llm.predict("hi!", callbacks=[handler])
+
+    for req in decode_requests(httpx.get_requests()):
+        assert req["properties"]["__langchainVersion"] == CURRENT_LANGCHAIN_VERSION
+        assert req["properties"]["__langchainLanguage"] == "python"
+
+
+def test_callback_resets_trace_id_between_calls(httpx, handler):
+    llm = OpenAI()
+
+    # First call
+    llm.predict("hi!", callbacks=[handler])
+    # Second call
+    llm.predict("hello!", callbacks=[handler])
+
+    requests = decode_requests(httpx.get_requests())
+
+    messages = [req["message"] for req in requests]
+    trace_ids = [req["traceId"] for req in requests]
+
+    assert messages == [
+        # First call
+        "langchain.llm.start",
+        "langchain.llm.end",
+        # Second call
+        "langchain.llm.start",
+        "langchain.llm.end",
+    ]
+
+    assert all(trace_id is not None for trace_id in trace_ids)
+
+    # First two trace ids should be the same
+    assert trace_ids[0] == trace_ids[1]
+    # Second two trace ids should be the same
+    assert trace_ids[2] == trace_ids[3]
+    # First and second trace ids should be different
+    assert trace_ids[0] != trace_ids[2]
+
+
+def test_callback_respects_trace_id_set_by_user(httpx, handler):
+    mock_trace_id = str(uuid.uuid4())
+    handler.tracer.set_trace_id(mock_trace_id)
+
+    llm = OpenAI()
+    llm.predict("hi!", callbacks=[handler])
+
+    handler.tracer.send_event("my.custom.event")
+
+    requests = decode_requests(httpx.get_requests())
+
+    assert [
+        ("langchain.llm.start", mock_trace_id),
+        ("langchain.llm.end", mock_trace_id),
+        ("my.custom.event", mock_trace_id),
+    ] == [(req["message"], req["traceId"]) for req in requests]
+
+
+def test_callback_with_agent_and_tools(httpx, handler):
     llm = OpenAI(temperature=0)
     llm_math_chain = LLMMathChain.from_llm(llm)
     tools = [
@@ -38,11 +106,10 @@ def test_callback_handler(httpx_mock):
     agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION)
     agent.run("What is 2+2?", callbacks=[handler])
 
-    requests = decode_requests(httpx_mock.get_requests())
+    requests = decode_requests(httpx.get_requests())
 
     messages = [req["message"] for req in requests]
     trace_ids = [req["traceId"] for req in requests]
-    property_payloads = [req["properties"] for req in requests]
 
     assert messages == [
         "langchain.chain.start",
@@ -70,7 +137,3 @@ def test_callback_handler(httpx_mock):
     # All trace_ids should be the same and should not be None
     assert trace_ids[0] is not None
     assert len(set(trace_ids)) == 1
-
-    for payload in property_payloads:
-        assert payload["__langchainVersion"] == CURRENT_LANGCHAIN_VERSION
-        assert payload["__langchainLanguage"] == "python"
