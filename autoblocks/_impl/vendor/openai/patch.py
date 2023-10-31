82b3ed458aaf3786ecb45274ecd8b78f90ffc5b6
Nicole White <nicole@autoblocks.ai>
2023-10-30 22:45:30 -0400

    Update messages

--- autoblocks/_impl/vendor/openai/patch.py
+++ autoblocks/_impl/vendor/openai/patch.py
@@ -99,11 +99,17 @@ def trace_openai(properties: Optional[Dict] = None, called=[False]) -> Autoblock
     try:
         import openai
     except ImportError:
-        raise ImportError("You must have openai installed in order to use the trace_openai function.")
+        raise ImportError(
+            "You must have openai installed in order to use the trace_openai function. "
+            "Install it with `pip install openai`."
+        )
     try:
         import wrapt
     except ImportError:
-        raise ImportError("You must have wrapt installed in order to use the trace_openai function.")
+        raise ImportError(
+            "You must have wrapt installed in order to use the trace_openai function. "
+            "Install it with `pip install wrapt`."
+        )
 
     if properties:
         tracer.update_properties(properties)
