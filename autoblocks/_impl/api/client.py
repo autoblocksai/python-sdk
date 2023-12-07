afab2ca4f5f612e5b6413f6c8de50aa16598eb5c
Adam Nolte <adam@autoblocks.ai>
2023-12-06 22:45:04 -0600

    fix imports

--- autoblocks/_impl/api/client.py
+++ autoblocks/_impl/api/client.py
@@ -11,14 +11,14 @@ from typing import Union
 import httpx
 
 from autoblocks._impl.api.models import AbsoluteTimeFilter
+from autoblocks._impl.api.models import Dataset
+from autoblocks._impl.api.models import DatasetItem
 from autoblocks._impl.api.models import Event
 from autoblocks._impl.api.models import RelativeTimeFilter
 from autoblocks._impl.api.models import Trace
 from autoblocks._impl.api.models import TraceFilter
 from autoblocks._impl.api.models import TracesResponse
 from autoblocks._impl.api.models import View
-from autoblocks._impl.api.models import Dataset
-from autoblocks._impl.api.models import DatasetItem
 from autoblocks._impl.config.constants import API_ENDPOINT
 
 log = logging.getLogger(__name__)
--- autoblocks/api/models.py
+++ autoblocks/api/models.py
@@ -1,4 +1,6 @@
 from autoblocks._impl.api.models import AbsoluteTimeFilter  # noqa: F401
+from autoblocks._impl.api.models import Dataset  # noqa: F401
+from autoblocks._impl.api.models import DatasetItem  # noqa: F401
 from autoblocks._impl.api.models import Event  # noqa: F401
 from autoblocks._impl.api.models import EventFilter  # noqa: F401
 from autoblocks._impl.api.models import EventFilterOperator  # noqa: F401
@@ -9,5 +11,3 @@ from autoblocks._impl.api.models import TraceFilter  # noqa: F401
 from autoblocks._impl.api.models import TraceFilterOperator  # noqa: F401
 from autoblocks._impl.api.models import TracesResponse  # noqa: F401
 from autoblocks._impl.api.models import View  # noqa: F401
-from autoblocks._impl.api.models import Dataset  # noqa: F401
-from autoblocks._impl.api.models import DatasetItem  # noqa: F401
