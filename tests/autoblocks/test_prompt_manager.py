54a1fe2bb7d2bcbf0e584775fd4a73d7e06f7528
Nicole White <nicole@autoblocks.ai>
2024-04-25 21:31:10 -0400

    snapshots -> revisions

--- tests/autoblocks/test_prompt_manager.py
+++ tests/autoblocks/test_prompt_manager.py
@@ -8,7 +8,7 @@ import pydantic
 import pytest
 
 from autoblocks._impl.config.constants import API_ENDPOINT
-from autoblocks._impl.prompts.error import IncompatiblePromptSnapshotError
+from autoblocks._impl.prompts.error import IncompatiblePromptRevisionError
 from autoblocks.prompts.context import PromptExecutionContext
 from autoblocks.prompts.manager import AutoblocksPromptManager
 from autoblocks.prompts.renderer import TemplateRenderer
@@ -70,12 +70,12 @@ class MyPromptManager(
     {
         "AUTOBLOCKS_API_KEY": "mock-api-key",
         "AUTOBLOCKS_CLI_SERVER_ADDRESS": MOCK_CLI_SERVER_ADDRESS,
-        "AUTOBLOCKS_PROMPT_SNAPSHOTS": json.dumps({MyPromptManager.__prompt_id__: "mock-snapshot-id"}),
+        "AUTOBLOCKS_PROMPT_REVISIONS": json.dumps({MyPromptManager.__prompt_id__: "mock-revision-id"}),
     },
 )
-def test_uses_prompt_snapshot(httpx_mock):
+def test_uses_prompt_revision(httpx_mock):
     httpx_mock.add_response(
-        url=f"{API_ENDPOINT}/prompts/my-prompt-id/snapshots/mock-snapshot-id/validate",
+        url=f"{API_ENDPOINT}/prompts/my-prompt-id/revisions/mock-revision-id/validate",
         method="POST",
         match_headers={"Authorization": "Bearer mock-api-key"},
         match_content=make_expected_body(
@@ -85,11 +85,10 @@ def test_uses_prompt_snapshot(httpx_mock):
         ),
         json=dict(
             id="my-prompt-id",
-            version="snapshot:mock-snapshot-id",
+            version="revision:mock-revision-id",
             templates=[
                 dict(
                     id="my-template",
-                    version="snapshot:mock-snapshot-id",
                     template="Hello, {{ name }}! The weather is {{ weather }} today!!!",
                 ),
             ],
@@ -109,12 +108,12 @@ def test_uses_prompt_snapshot(httpx_mock):
     {
         "AUTOBLOCKS_API_KEY": "mock-api-key",
         "AUTOBLOCKS_CLI_SERVER_ADDRESS": MOCK_CLI_SERVER_ADDRESS,
-        "AUTOBLOCKS_PROMPT_SNAPSHOTS": json.dumps({MyPromptManager.__prompt_id__: "mock-snapshot-id"}),
+        "AUTOBLOCKS_PROMPT_REVISIONS": json.dumps({MyPromptManager.__prompt_id__: "mock-revision-id"}),
     },
 )
-def test_uses_prompt_snapshot_when_version_is_latest(httpx_mock):
+def test_uses_prompt_revision_when_version_is_latest(httpx_mock):
     httpx_mock.add_response(
-        url=f"{API_ENDPOINT}/prompts/my-prompt-id/snapshots/mock-snapshot-id/validate",
+        url=f"{API_ENDPOINT}/prompts/my-prompt-id/revisions/mock-revision-id/validate",
         method="POST",
         match_headers={"Authorization": "Bearer mock-api-key"},
         match_content=make_expected_body(
@@ -124,11 +123,10 @@ def test_uses_prompt_snapshot_when_version_is_latest(httpx_mock):
         ),
         json=dict(
             id="my-prompt-id",
-            version="snapshot:mock-snapshot-id",
+            version="revision:mock-revision-id",
             templates=[
                 dict(
                     id="my-template",
-                    version="snapshot:mock-snapshot-id",
                     template="Hello, {{ name }}! The weather is {{ weather }} today!!!",
                 ),
             ],
@@ -148,10 +146,10 @@ def test_uses_prompt_snapshot_when_version_is_latest(httpx_mock):
     {
         "AUTOBLOCKS_API_KEY": "mock-api-key",
         "AUTOBLOCKS_CLI_SERVER_ADDRESS": MOCK_CLI_SERVER_ADDRESS,
-        "AUTOBLOCKS_PROMPT_SNAPSHOTS": json.dumps({"some-other-prompt-id": "mock-snapshot-id"}),
+        "AUTOBLOCKS_PROMPT_REVISIONS": json.dumps({"some-other-prompt-id": "mock-revision-id"}),
     },
 )
-def test_uses_configured_version_if_snapshot_is_for_different_prompt(httpx_mock):
+def test_uses_configured_version_if_revision_is_for_different_prompt(httpx_mock):
     httpx_mock.add_response(
         url=f"{API_ENDPOINT}/prompts/my-prompt-id/major/1/minor/0",
         method="GET",
@@ -162,7 +160,6 @@ def test_uses_configured_version_if_snapshot_is_for_different_prompt(httpx_mock)
             templates=[
                 dict(
                     id="my-template",
-                    version="1.0",
                     template="Hello, {{ name }}! The weather is {{ weather }} today.",
                 ),
             ],
@@ -182,12 +179,12 @@ def test_uses_configured_version_if_snapshot_is_for_different_prompt(httpx_mock)
     {
         "AUTOBLOCKS_API_KEY": "mock-api-key",
         "AUTOBLOCKS_CLI_SERVER_ADDRESS": MOCK_CLI_SERVER_ADDRESS,
-        "AUTOBLOCKS_PROMPT_SNAPSHOTS": json.dumps({MyPromptManager.__prompt_id__: "mock-snapshot-id"}),
+        "AUTOBLOCKS_PROMPT_REVISIONS": json.dumps({MyPromptManager.__prompt_id__: "mock-revision-id"}),
     },
 )
 def test_raises_if_prompt_is_incompatible(httpx_mock):
     httpx_mock.add_response(
-        url=f"{API_ENDPOINT}/prompts/my-prompt-id/snapshots/mock-snapshot-id/validate",
+        url=f"{API_ENDPOINT}/prompts/my-prompt-id/revisions/mock-revision-id/validate",
         method="POST",
         match_headers={"Authorization": "Bearer mock-api-key"},
         match_content=make_expected_body(
@@ -195,16 +192,16 @@ def test_raises_if_prompt_is_incompatible(httpx_mock):
                 majorVersion=1,
             ),
         ),
-        # If the snapshot prompt ID matches the manager's prompt ID but the snapshot is
+        # If the revision's prompt ID matches the manager's prompt ID but the revision is
         # incompatible, the endpoint will return a 409.
         status_code=HTTPStatus.CONFLICT,
     )
 
-    with pytest.raises(IncompatiblePromptSnapshotError) as exc:
+    with pytest.raises(IncompatiblePromptRevisionError) as exc:
         MyPromptManager(MyMinorVersion.v0)
 
     assert str(exc.value) == (
-        "Can't override 'MyPromptManager' with prompt snapshot 'mock-snapshot-id' "
+        "Can't override prompt 'MyPromptManager' with revision 'mock-revision-id' "
         "because it is not compatible with major version '1'."
     )
 
@@ -216,10 +213,10 @@ def test_raises_if_prompt_is_incompatible(httpx_mock):
     {
         "AUTOBLOCKS_API_KEY": "mock-api-key",
         # Note that AUTOBLOCKS_CLI_SERVER_ADDRESS is not set
-        "AUTOBLOCKS_PROMPT_SNAPSHOTS": json.dumps({MyPromptManager.__prompt_id__: "mock-snapshot-id"}),
+        "AUTOBLOCKS_PROMPT_REVISIONS": json.dumps({MyPromptManager.__prompt_id__: "mock-revision-id"}),
     },
 )
-def test_ignores_snapshot_id_if_not_in_test_run_context(httpx_mock):
+def test_ignores_revision_id_if_not_in_test_run_context(httpx_mock):
     httpx_mock.add_response(
         url=f"{API_ENDPOINT}/prompts/my-prompt-id/major/1/minor/0",
         method="GET",
@@ -230,7 +227,6 @@ def test_ignores_snapshot_id_if_not_in_test_run_context(httpx_mock):
             templates=[
                 dict(
                     id="my-template",
-                    version="1.0",
                     template="Hello, {{ name }}! The weather is {{ weather }} today.",
                 ),
             ],
