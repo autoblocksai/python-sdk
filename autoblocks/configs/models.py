4522153ecf309aba4840c23eeadb8cfbaa18f1c2
Adam Nolte <ajnolte12@gmail.com>
2024-04-29 14:25:06 -0500

    remove export

--- autoblocks/configs/models.py
+++ autoblocks/configs/models.py
@@ -3,11 +3,9 @@ from autoblocks._impl.configs.models import DangerouslyUseUndeployedWithRevision
 from autoblocks._impl.configs.models import LatestDangerouslyUseUndeployed
 from autoblocks._impl.configs.models import LatestRemoteConfig
 from autoblocks._impl.configs.models import RemoteConfig
-from autoblocks._impl.configs.models import RemoteConfigResponse
 from autoblocks._impl.configs.models import RemoteConfigWithVersion
 
 __all__ = [
-    "RemoteConfigResponse",
     "RemoteConfig",
     "LatestRemoteConfig",
     "RemoteConfigWithVersion",
