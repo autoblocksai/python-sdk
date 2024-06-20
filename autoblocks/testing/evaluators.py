5dc1552179281570f2923b500cc232ccef29936a
Adam Nolte <ajnolte12@gmail.com>
2024-06-20 16:49:55 -0500

    export

--- autoblocks/testing/evaluators.py
+++ autoblocks/testing/evaluators.py
@@ -1,5 +1,4 @@
+from autoblocks._impl.testing.evaluators.battle import Battle
 from autoblocks._impl.testing.evaluators.has_all_substrings import HasAllSubstrings
 
-__all__ = [
-    "HasAllSubstrings",
-]
+__all__ = ["HasAllSubstrings", "Battle"]
