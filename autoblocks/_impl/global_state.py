eeeaab4e0de3a9187432e10faeefb79628686e27
Adam Nolte <ajnolte12@gmail.com>
2024-09-04 13:41:29 -0500

    Verify test fails

--- autoblocks/_impl/global_state.py
+++ autoblocks/_impl/global_state.py
@@ -19,8 +19,8 @@ _client: Optional[httpx.AsyncClient] = None
 _sync_client: Optional[httpx.Client] = None
 _background_tasks: Set[AnyTask] = set()
 _main_thread_has_finished: bool = False
-_test_run_api_semaphore: Optional[asyncio.Semaphore] = None
-_github_comment_semaphore: Optional[asyncio.Semaphore] = None
+_test_run_api_semaphore = asyncio.Semaphore(value=10)
+_github_comment_semaphore = asyncio.Semaphore(value=1)
 
 
 def _run_event_loop(_event_loop: asyncio.AbstractEventLoop) -> None:
@@ -125,10 +125,10 @@ def init() -> None:
     # Important that these get initialized in the background event loop that testing uses
     # Otherwise we will see errors like Future <Future pending> attached to a different loop in Python 3.9
     # See https://stackoverflow.com/questions/55918048/asyncio-semaphore-runtimeerror-task-got-future-attached-to-a-different-loop
-    asyncio.run_coroutine_threadsafe(
-        init_semaphores(),
-        _background_event_loop,
-    ).result()
+    # asyncio.run_coroutine_threadsafe(
+    #     init_semaphores(),
+    #     _background_event_loop,
+    # ).result()
 
     _started = True
 
