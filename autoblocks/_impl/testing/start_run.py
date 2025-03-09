4950afc1ee496ad0b1de0944c4d1deaf17b95fd1
Adam Nolte <ajnolte12@gmail.com>
2025-03-08 20:47:24 -0600

    Add some comments

--- autoblocks/_impl/testing/start_run.py
+++ autoblocks/_impl/testing/start_run.py
@@ -6,8 +6,14 @@ from autoblocks._impl.util import cuid_generator
 
 
 def start_run(message: Optional[str] = None) -> None:
+    """
+    Can be used to start a test run when using the auto tracer.
+    """
     test_run_context_var.set(TestRunContext(run_id=cuid_generator(), run_message=message))
 
 
 def end_run() -> None:
+    """
+    Used in conjunction with `start_run` to end a test run when using the auto tracer.
+    """
     test_run_context_var.set(None)
