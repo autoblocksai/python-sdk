2c59bf899049a3903d8f04172727afb81c58401f
Adam Nolte <adam@autoblocks.ai>
2025-06-25 13:47:57 -0500

    Fix typo (#361)

--- autoblocks/_impl/config/constants.py
+++ autoblocks/_impl/config/constants.py
@@ -1,7 +1,7 @@
 from autoblocks._impl.util import AutoblocksEnvVar
 
 API_ENDPOINT = "https://api.autoblocks.ai"
-API_ENDPOINT_V2 = AutoblocksEnvVar.V2_API_ENDPOINT_V2.get() or "https://api-v2.autoblocks.ai"
+API_ENDPOINT_V2 = AutoblocksEnvVar.V2_API_ENDPOINT.get() or "https://api-v2.autoblocks.ai"
 INGESTION_ENDPOINT = "https://ingest-event.autoblocks.ai"
 REVISION_LATEST = "latest"
 REVISION_UNDEPLOYED = "undeployed"
